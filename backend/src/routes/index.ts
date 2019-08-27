00500a449d98323ced12ae41c438131df2996155
opzyra <opzyra@gmail.com>
2019-08-27 21:01:57 +0900

    Implements posts api

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import check from "./check";
+import posts from "./posts";
 
 const router = express.Router();
 
 router.use("/check", check);
+router.use("/posts", posts);
 
 export default router;
