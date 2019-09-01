c126e0a7f9abcfeb97cdca9fd76556be39b6a74d
opzyra <opzyra@gmail.com>
2019-09-01 14:04:35 +0900

    Add tistory parser

--- backend/src/batch/index.ts
+++ backend/src/batch/index.ts
@@ -4,14 +4,16 @@ import okky from "./okky";
 import velog from "./velog";
 import brunch from "./brunch";
 import medium from "./medium";
+import tistory from "./tistory";
 import github from "./github";
 
 const parsing = () => {
-  cron.schedule("0 1 0 * * *", okky);
-  cron.schedule("0 3 0 * * *", velog);
-  cron.schedule("0 5 0 * * *", brunch);
-  cron.schedule("0 7 0 * * *", github);
-  cron.schedule("0 9 0 * * *", medium);
+  cron.schedule("0 5 1 * * *", okky);
+  cron.schedule("0 10 1 * * *", velog);
+  cron.schedule("0 15 1 * * *", brunch);
+  cron.schedule("0 20 1 * * *", github);
+  cron.schedule("0 25 1 * * *", tistory);
+  cron.schedule("0 30 1 * * *", medium);
 };
 
 export default parsing;
